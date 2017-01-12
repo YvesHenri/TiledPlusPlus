6cab0ca495614a52b5724c9046613b0be4eaa2b8
YvesHenri <yveshenricalaci@hotmail.com>
2017-01-12 13:31:15 -0300

    First commit containing all the dependencies: they will be built within this project. Users will not need to care about them (third-party dependencies)

---
+++ TiledReader/Includes/Macros/Exporter.h
@@ -0,0 +1,35 @@
+#ifndef TMX_MACROS_EXPORTER_H_
+#define TMX_MACROS_EXPORTER_H_
+
+#include "OS.h"
+
+// If TILEDPP_STATIC is defined as a "Preprocessor Definition", static libs are created instead (does not require .dll)
+#if !defined(TILEDPP_STATIC)
+	// Windows
+	#if defined(TILEDPP_OS_WINDOWS)
+		#define TILEDPP_API_EXPORT __declspec(dllexport)
+		#define TILEDPP_API_IMPORT __declspec(dllimport)
+
+		// For Visual C++ compilers, we also need to turn off this annoying C4251 warning
+		#ifdef _MSC_VER
+			#pragma warning(disable: 4251)
+		#endif
+	// Linux, FreeBSD, Mac OS X
+	#else
+		#if __GNUC__ >= 4
+			// GCC 4 has special keywords for showing/hidding symbols, the same keyword is used for both importing and exporting
+			#define TILEDPP_API_EXPORT __attribute__ ((__visibility__ ("default")))
+			#define TILEDPP_API_IMPORT __attribute__ ((__visibility__ ("default")))
+		#else
+			// GCC < 4 has no mechanism to explicitely hide symbols, everything's exported
+			#define TILEDPP_API_EXPORT
+			#define TILEDPP_API_IMPORT
+		#endif // __GNUC__ >= 4
+	#endif // defined(TILEDPP_OS_WINDOWS)
+#else
+	// Static build doesn't need import/export macros
+	#define TILEDPP_API_EXPORT
+	#define TILEDPP_API_IMPORT
+#endif // !defined(TILEDPP_STATIC)
+
+#endif // ifndef TMX_MACROS_EXPORTER_H_
---
+++ TiledReader/Includes/Macros/OS.h
@@ -0,0 +1,45 @@
+#ifndef TMX_MACROS_OS_H_
+#define TMX_MACROS_OS_H_
+
+////////////////////////////////////////////////////////////
+// Identify the operating system
+// see http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system
+////////////////////////////////////////////////////////////
+#if defined(_WIN32)
+
+	// Windows
+	#define TILEDPP_OS_WINDOWS
+
+#elif defined(__APPLE__) && defined(__MACH__)
+
+	#include "TargetConditionals.h" // Requires "Command Line Tools for Xcode" package
+
+	// Apple platform, see which one it is
+	#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+		#define TILEDPP_OS_IOS
+	#elif TARGET_OS_MAC
+		#define TILEDPP_OS_MACOS
+	#else
+		#error This Apple operating system is not supported by SFML library
+	#endif // TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+
+#elif defined(__unix__)
+
+	// UNIX system, see which one it is
+	#if defined(__ANDROID__)
+		#define TILEDPP_OS_ANDROID
+	#elif defined(__linux__)
+		#define TILEDPP_OS_LINUX
+	#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+		#define TILEDPP_OS_FREEBSD
+	#else
+		#error This UNIX operating system is not supported by SFML library
+	#endif // defined(__ANDROID__)
+
+#else
+
+	#error This operating system is not supported by SFML library
+
+#endif // defined(_WIN32)
+
+#endif // ifndef TMX_MACROS_OS_H_
